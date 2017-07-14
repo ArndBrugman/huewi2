85443fa6c6d65cf830124d046445e4c5f580d342
Arnd Brugman <arndbrugman@gmail.com>
2017-07-14 19:06:57 +0200

    Adjuste mock data and set step to 16 on sliders

--- src/app/huewi-lights/huewi-lights.mock.ts
+++ src/app/huewi-lights/huewi-lights.mock.ts
@@ -21,11 +21,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:b5:90:08-0b",
-        "swversion": "5.23.1.13452",
-        "id": "2",
-        "HTMLColor": "#f3b350",
-        "__key": "2",
-        "$$hashKey": "object:7260"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -49,67 +45,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:b5:a8:73-0b",
-        "swversion": "5.23.1.13452",
-        "id": "3",
-        "HTMLColor": "#f3b350",
-        "__key": "3",
-        "$$hashKey": "object:7259"
-    },
-    {
-        "state": {
-            "on": false,
-            "bri": 254,
-            "hue": 34225,
-            "sat": 167,
-            "effect": "none",
-            "xy": [0.3354, 0.3436],
-            "ct": 184,
-            "alert": "none",
-            "colormode": "ct",
-            "reachable": true
-        },
-        "swupdate": {
-            "state": "noupdates",
-            "lastinstall": null
-        },
-        "type": "Extended color light",
-        "name": "Door Front",
-        "modelid": "LCT001",
-        "manufacturername": "Philips",
-        "uniqueid": "00:17:88:01:00:b1:ad:91-0b",
-        "swversion": "5.23.1.13452",
-        "id": "4",
-        "HTMLColor": "#fefcea",
-        "__key": "4",
-        "$$hashKey": "object:7256"
-    },
-    {
-        "state": {
-            "on": false,
-            "bri": 254,
-            "hue": 34225,
-            "sat": 167,
-            "effect": "none",
-            "xy": [0.3354, 0.3436],
-            "ct": 184,
-            "alert": "none",
-            "colormode": "ct",
-            "reachable": true
-        },
-        "swupdate": {
-            "state": "noupdates",
-            "lastinstall": null
-        },
-        "type": "Extended color light",
-        "name": "Door Back",
-        "modelid": "LCT001",
-        "manufacturername": "Philips",
-        "uniqueid": "00:17:88:01:00:bf:6e:cf-0b",
-        "swversion": "5.23.1.13452",
-        "id": "5",
-        "HTMLColor": "#fefcea",
-        "__key": "5",
-        "$$hashKey": "object:7255"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -133,11 +69,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:fe:5c:f2-0b",
-        "swversion": "5.23.1.13452",
-        "id": "6",
-        "HTMLColor": "#f3b350",
-        "__key": "6",
-        "$$hashKey": "object:7263"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -161,11 +93,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:fe:60:c8-0b",
-        "swversion": "5.23.1.13452",
-        "id": "7",
-        "HTMLColor": "#f3b350",
-        "__key": "7",
-        "$$hashKey": "object:7261"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -189,11 +117,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:fe:58:8e-0b",
-        "swversion": "5.23.1.13452",
-        "id": "8",
-        "HTMLColor": "#f3b350",
-        "__key": "8",
-        "$$hashKey": "object:7262"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -217,11 +141,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:dd:1e:d1-0b",
-        "swversion": "5.50.1.19085",
-        "id": "9",
-        "HTMLColor": "#f3b350",
-        "__key": "9",
-        "$$hashKey": "object:7258"
+        "swversion": "5.50.1.19085"
     },
     {
         "state": {
@@ -245,11 +165,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:b3:9e:63-0b",
-        "swversion": "5.23.1.13452",
-        "id": "10",
-        "HTMLColor": "#f3b350",
-        "__key": "10",
-        "$$hashKey": "object:7257"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -273,11 +189,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:e0:2f:67-0b",
-        "swversion": "5.23.1.13452",
-        "id": "11",
-        "HTMLColor": "#593f17",
-        "__key": "11",
-        "$$hashKey": "object:7253"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -301,11 +213,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LCT001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:00:e0:64:a6-0b",
-        "swversion": "5.23.1.13452",
-        "id": "12",
-        "HTMLColor": "#593f17",
-        "__key": "12",
-        "$$hashKey": "object:7254"
+        "swversion": "5.23.1.13452"
     },
     {
         "state": {
@@ -325,11 +233,7 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LTW001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:02:04:c6:5a-0b",
-        "swversion": "5.50.1.19085",
-        "id": "13",
-        "HTMLColor": "#57400e",
-        "__key": "13",
-        "$$hashKey": "object:7252"
+        "swversion": "5.50.1.19085"
     },
     {
         "state": {
@@ -349,100 +253,6 @@ export const HUEWI_LIGHTS_MOCK = [
         "modelid": "LTW001",
         "manufacturername": "Philips",
         "uniqueid": "00:17:88:01:02:04:c7:38-0b",
-        "swversion": "5.50.1.19085",
-        "id": "14",
-        "HTMLColor": "#57400e",
-        "__key": "14",
-        "$$hashKey": "object:7251"
-    },
-    {
-        "state": {
-            "on": false,
-            "bri": 38,
-            "hue": 7838,
-            "sat": 189,
-            "effect": "none",
-            "xy": [0.4931, 0.4151],
-            "ct": 431,
-            "alert": "none",
-            "colormode": "ct",
-            "reachable": true
-        },
-        "swupdate": {
-            "state": "noupdates",
-            "lastinstall": null
-        },
-        "type": "Extended color light",
-        "name": "Sleep Arnd",
-        "modelid": "LCT010",
-        "manufacturername": "Philips",
-        "uniqueid": "00:17:88:01:02:79:99:6b-0b",
-        "swversion": "1.15.2_r19181",
-        "swconfigid": "F921C859",
-        "productid": "Philips-LCT010-1-A19ECLv4",
-        "id": "15",
-        "HTMLColor": "#82631f",
-        "__key": "15",
-        "$$hashKey": "object:7264"
-    },
-    {
-        "state": {
-            "on": false,
-            "bri": 38,
-            "hue": 7838,
-            "sat": 189,
-            "effect": "none",
-            "xy": [0.4931, 0.4151],
-            "ct": 431,
-            "alert": "none",
-            "colormode": "ct",
-            "reachable": false
-        },
-        "swupdate": {
-            "state": "noupdates",
-            "lastinstall": null
-        },
-        "type": "Extended color light",
-        "name": "Sleep Room",
-        "modelid": "LCT010",
-        "manufacturername": "Philips",
-        "uniqueid": "00:17:88:01:02:79:97:c7-0b",
-        "swversion": "1.15.2_r19181",
-        "swconfigid": "F921C859",
-        "productid": "Philips-LCT010-1-A19ECLv4",
-        "id": "16",
-        "HTMLColor": "#82631f",
-        "__key": "16",
-        "$$hashKey": "object:7266"
-    },
-    {
-        "state": {
-            "on": false,
-            "bri": 38,
-            "hue": 7838,
-            "sat": 189,
-            "effect": "none",
-            "xy": [0.4931, 0.4151],
-            "ct": 431,
-            "alert": "none",
-            "colormode": "ct",
-            "reachable": true
-        },
-        "swupdate": {
-            "state": "noupdates",
-            "lastinstall": null
-        },
-        "type": "Extended color light",
-        "name": "Sleep Jurdy",
-        "modelid": "LCT010",
-        "manufacturername": "Philips",
-        "uniqueid": "00:17:88:01:02:79:99:81-0b",
-        "swversion": "1.15.2_r19181",
-        "swconfigid": "F921C859",
-        "productid": "Philips-LCT010-1-A19ECLv4",
-        "id": "17",
-        "HTMLColor": "#82631f",
-        "__key": "17",
-        "$$hashKey": "object:7265"
+        "swversion": "5.50.1.19085"
     }
 ]

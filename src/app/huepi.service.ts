9679f9068d42f3fe9859c0c7e63795f193ec9338
Arnd Brugman <arndbrugman@gmail.com>
2017-07-19 13:37:50 +0200

    blend 2 of OneDrive f*ck *up

--- src/app/huepi.service.ts
+++ src/app/huepi.service.ts
@@ -1,53 +1,44 @@
 import { Injectable } from '@angular/core';
-import * as axios from 'axios'
+import axios from 'axios';
 import { huepi } from 'assets/huepi.js';
+huepi.http = axios.create();
+import { HUEPI_MOCK } from './huepi.mock'
 
 @Injectable()
 export class HuepiService {
   private MyHue;
   
-  groups : Array<any>;
-  lights : Array<any>;
-  rules : Array<any>;
-  scenes : Array<any>;
-  schedules : Array<any>;
-  sensors : Array<any>;
+  public groups : Array<any>;
+  public lights : Array<any>;
+  public rules : Array<any>;
+  public scenes : Array<any>;
+  public schedules : Array<any>;
+  public sensors : Array<any>;
 
   constructor() {
     this.MyHue = new huepi();
 
-    this.MyHue.Lights['1'] = {name: 'Demo Light'};
-    this.MyHue.Lights['2'] = {name: 'Living Light'};
-    this.MyHue.Lights['3'] = {name: 'Dining Light'};
-    this.MyHue.Groups['1'] = {name: 'Demo Group', type: 'LightGroup'};
-    this.MyHue.Groups['2'] = {name: 'Living Group', type: 'LightGroup'};
-    this.MyHue.Groups['3'] = {name: 'Dining Group', type: 'LightGroup'};
-    this.MyHue.Groups['11'] = {name: 'Demo Room', type: 'Room'};
-    this.MyHue.Groups['12'] = {name: 'Living Room', type: 'Room'};
-    this.MyHue.Groups['13'] = {name: 'Dining Room', type: 'Room'};
-    this.MyHue.Groups['1'].lights=this.MyHue.Groups['2'].lights=this.MyHue.Groups['3'].lights=["1","2"];
-    this.MyHue.Groups['11'].lights=this.MyHue.Groups['12'].lights=this.MyHue.Groups['13'].lights=["1","2"];
-    this.MyHue.Lights['1'].state={'on':true,'bri':141,'xy':[0.5,0.4],'colormode':'xy','reachable':true};
-    this.MyHue.Lights['2'].state={'on':true,'bri':152,'xy':[0.4,0.5],'colormode':'xy','reachable':true};
-    this.MyHue.Lights['3'].state={'on':true,'bri':163,'xy':[0.5,0.5],'colormode':'xy','reachable':true};
-    this.MyHue.Groups['1'].action={'on':true,'bri':131,'xy':[0.5,0.4],'colormode':'xy','reachable':true};
-    this.MyHue.Groups['2'].action={'on':true,'bri':142,'xy':[0.4,0.5],'colormode':'xy','reachable':true};
-    this.MyHue.Groups['3'].action={'on':true,'bri':153,'xy':[0.5,0.5],'colormode':'xy','reachable':true};
-    this.MyHue.Groups['11'].action={'on':true,'bri':161,'xy':[0.5,0.4],'colormode':'xy','reachable':true};
-    this.MyHue.Groups['12'].action={'on':true,'bri':172,'xy':[0.4,0.5],'colormode':'xy','reachable':true};
-    this.MyHue.Groups['13'].action={'on':true,'bri':183,'xy':[0.5,0.5],'colormode':'xy','reachable':true};
+    this.MyHue['Lights'] = HUEPI_MOCK['lights'];
+    this.MyHue['Groups'] = HUEPI_MOCK['groups'];
+
+    this.MyHue.PortalDiscoverLocalBridges().then(() => {
+      this.MyHue.BridgeGetConfig().then(() => {
+        this.MyHue.BridgeGetData().then(() => {
+          console.log('Data Received');
+          this.update();
+        });
+      });
+    });
+
     this.update();
-    
-    setInterval( () => { this.update()}, 666);
   }
 
   update () {
-    console.log('Update', JSON.stringify(this.MyHue));
     
     let groups = [];
     if (this.MyHue.Groups) {
       Object.keys(this.MyHue.Groups).forEach((key) => {
-  this.MyHue.Groups[key].action.bri = 3 * Date.now() % 255;
+        this.MyHue.Groups[key].__key = key;
         groups.push(this.MyHue.Groups[key]);
       })
     }
@@ -56,10 +47,14 @@ export class HuepiService {
     let lights = [];
     if (this.MyHue.Lights) {
       Object.keys(this.MyHue.Lights).forEach((key) => {
-  this.MyHue.Lights[key].state.bri = 777 * Date.now() % 255;
+        this.MyHue.Lights[key].__key = key;
         lights.push(this.MyHue.Lights[key]);
       })
     }
     this.lights = lights;
+
+    console.log('Update', JSON.stringify(this.groups));
   }
+
+  
 }

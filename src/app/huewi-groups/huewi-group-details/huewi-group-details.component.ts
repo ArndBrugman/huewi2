f54e45a99c3891e21edc0bf88017dd180af0f244
Arnd Brugman <arndbrugman@gmail.com>
2017-07-21 17:52:15 +0200

    Group & Light Details preset buttons working

--- src/app/huewi-groups/huewi-group-details/huewi-group-details.component.ts
+++ src/app/huewi-groups/huewi-group-details/huewi-group-details.component.ts
@@ -16,4 +16,42 @@ export class HuewiGroupDetailsComponent implements OnInit {
   ngOnInit() {
   }
 
+  setCTBrightness(group, CT, Brightness) {
+    this.huepiService.MyHue.GroupOn(group.__key);
+    this.huepiService.MyHue.GroupSetCT(group.__key, CT);
+    this.huepiService.MyHue.GroupSetBrightness(group.__key, Brightness);
+  }
+
+  relax(group) {
+    this.setCTBrightness(group, 346, 254);
+  }
+
+  reading(group) {
+    this.setCTBrightness(group, 346, 254);
+  }
+
+  concentrate(group) {
+    this.setCTBrightness(group, 234, 254);
+  }
+
+  energize(group) {
+    this.setCTBrightness(group, 153, 254);
+  }
+
+  bright(group) {
+    this.setCTBrightness(group, 367, 254);
+  }
+
+  dimmed(group) {
+    this.setCTBrightness(group, 365, 77);
+  }
+
+  nightLight(group) {
+    this.setCTBrightness(group, 500, 1);
+  }
+
+  goldenHour(group) {
+    this.setCTBrightness(group, 400, 125);
+  }
+
 }

a4dcdd19e56cda76fb45dd78424aaf2714eec7da
Arnd Brugman <arndbrugman@gmail.com>
2017-08-25 08:54:04 +0200

    Added Touch Sequence to About

--- src/app/huewi-about/huewi-about.component.ts
+++ src/app/huewi-about/huewi-about.component.ts
@@ -1,4 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
+
+import { Router } from '@angular/router';
 import { RoutingAnimations } from './../app-routing.animations';
 
 @Component({
@@ -9,10 +11,31 @@ import { RoutingAnimations } from './../app-routing.animations';
 })
 export class HuewiAboutComponent implements OnInit {
   @HostBinding('@RoutingAnimations') get RoutingAnimations() { return true };
+  @Input() touchSequence = ['swiperight', 'swipeleft', 'press'];
+  showNote = false;
+  touchPhase = 0;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.showNote = false;
+  }
+
+  touchEvent(action) {
+    if (action === this.touchSequence[this.touchPhase]) {
+      this.touchPhase++; // sequence is continueing
+    } else {
+      this.touchPhase = 0; // sequence is broken
+      // however, recheck action to validate if last action matches first touch action in touchSequence
+      if (action === this.touchSequence[this.touchPhase]) {
+        this.touchPhase++; // yes: sequence is broken by first item in sequence
+      }
+    }
+    console.log(action, this.touchPhase);
+    if (this.touchPhase === this.touchSequence.length) {
+      this.showNote = true;
+    }
   }
 
 }
